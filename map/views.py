afb5ea9f4b23bfe2afefd8d7a6c5f73c7617de82
Mizuhara Itsuki <110594150+Iccyan21@users.noreply.github.com>
2023-07-04 11:06:46 +0900

    Add Commit

--- map/views.py
+++ map/views.py
@@ -6,6 +6,9 @@ from rest_framework.views import APIView
 from .models import Place
 from .serializers import PlaceSerializer
 from rest_framework.response import Response
+from django.db.models import Q
+import googlemaps
+import os
 # Create your views here.
 
 # getでnameからdataを取得して、APIに送る
@@ -38,20 +41,62 @@ class PlaceDataIntAPI(APIView):
             return Response(status=404)
 
 # 場所をSerachする処理    
-class PlaceSearchAPIView(APIView):    
+class PlaceSearchAPIView(APIView):
     def get_queryset(self):
-        # get_querysetメソッドを呼び出して検索結果のクエリセットを取得
         query = self.request.GET.get('q', '')
-        return Place.objects.search_by_name(query)
+        # アニメのtitleに一致する場所を検索する
+        return Place.objects.filter(Q(name__icontains=query) | Q(animetitle__title__icontains=query))
 
     def get(self, request):
         queryset = self.get_queryset()
         serializer = PlaceSerializer(queryset, many=True)
-        
+
         data = serializer.data
 
         for item in data:
             item['placeimage'] = unquote(item['placeimage'])
 
         return Response(data)
-    
+
+from django.core.exceptions import ImproperlyConfigured
+import os
+
+class DirectionsView(APIView):
+    def get(self, request, *args, **kwargs):
+        # 環境変数からAPIキーを取得します
+        api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+        if not api_key:
+            raise ImproperlyConfigured("GOOGLE_MAPS_API_KEY must be set")
+
+        gmaps = googlemaps.Client(key=api_key)
+
+        # クエリパラメータから出発地と目的地を取得します
+        start = request.GET.get('start')
+        end = request.GET.get('end')
+        
+        if not start or not end:
+            return Response({"error": "start and end parameters are required."}, status=400)
+
+        # APIにリクエストを送信します
+        directions_result = gmaps.directions(start, end)
+
+        # レスポンスデータからルート情報だけを抽出します
+        if directions_result:
+            leg = directions_result[0]['legs'][0]  # Consider only the first route
+            steps = []
+            for step in leg['steps']:
+                steps.append({
+                    'start_location': step['start_location'],
+                    'end_location': step['end_location'],
+                    'html_instructions': step['html_instructions'],
+                    'distance': step['distance']['text'],
+                    'duration': step['duration']['text'],
+                })
+
+            # Return the first route's steps
+            return Response(steps)
+
+        # If no routes found
+        return Response({"error": "No routes found."}, status=404)
+
+
